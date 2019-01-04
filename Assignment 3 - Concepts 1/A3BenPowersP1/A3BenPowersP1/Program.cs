3acb11ccddc382bf3ee69682366614915b02e43e
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 15:18:54 -0500

    Add files via upload

---
+++ Assignment 3 - Concepts 1/A3BenPowersP1/A3BenPowersP1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Assignment 3 Part 1
+// Ben Powers
+// Student Number 7986250
+
+namespace A3BenPowersP1
+{
+    class Program
+    {
+        public static object Int { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            string menuSelectedByUser;
+            string exitString = "no";
+            
+            
+            Console.WriteLine("Ben Powers");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            do
+            {
+                
+
+                // even numbers
+                Console.WriteLine("1. for a list of even numbers");
+
+                // perfect squares
+                Console.WriteLine("2. for perfect squares");
+
+                // exits program
+                Console.WriteLine("3. to close the program");
+
+                //asks user to select a number
+                Console.WriteLine("Select a number:");
+
+                // even numbers (first option)
+                menuSelectedByUser = Console.ReadLine();
+                if (menuSelectedByUser == "1")
+                {
+                    Console.WriteLine("Type a number for a list of even numbers: ");
+                    string input = Console.ReadLine();
+                    int x;
+                    if (!int.TryParse(input, out x))
+                    {
+                        Console.WriteLine("You didn't enter number");
+                    }
+
+                    for(int i = 0; i < x*2; i+=2)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                // perfect squares (option 2)
+                else if (menuSelectedByUser == "2")
+                {
+                    double next = 1;
+                    Console.WriteLine("The square of 1 is 1");
+                    Console.WriteLine("Do you wish to continue?[Y/N]");
+                    string a = Console.ReadLine();
+
+                    if (a == "Y")
+                    {
+                        while (a == "Y")
+                        {
+                            next++;
+                            do
+                            {
+
+                                double result = next * next;
+                                Console.WriteLine("The square of " + next + " is " + result);
+                                next++;
+                                Console.WriteLine("Do you wish to continue?[Y/N]");
+                                a = Console.ReadLine();
+
+                            }
+                            while (a == "Y");
+                            Console.WriteLine("exit");
+
+                        }
+                    }
+             
+                } 
+                
+                // exit program (option 3)
+                else if (menuSelectedByUser == "3")
+                {
+                    Console.WriteLine("Do you want to exit the program? [yes/no]");
+                    exitString = Console.ReadLine();
+                    
+                }
+
+            }   while (exitString != "yes");
+            
+        }
+        
+        
+    }
+}
---
+++ Assignment 3 - Concepts 1/A3BenPowersP2/A3BenPowersP2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Assignment 3 Part 2
+// Ben Powers
+// Student Number 7986250
+
+namespace A3BenPowersP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            string homeMenu;
+            int numberOne;
+            int numberTwo;
+            string numString;
+            
+
+            Console.WriteLine("Ben Powers");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            do
+
+            {
+
+                // Home menu for math calculator
+
+                // add method
+                Console.WriteLine("Press 1 for addition");
+
+                // sub method
+                Console.WriteLine("Press 2 for subtraction");
+
+                // divide method
+                Console.WriteLine("Press 3 for division");
+
+                // multiply method
+                Console.WriteLine("Press 4 for multiplication");
+
+                Console.WriteLine("------------------------------------------------------------------------");
+
+                homeMenu = Console.ReadLine();
+
+                // adding two numbers
+                if (homeMenu == "1")
+                    try
+
+                    {
+                        Console.WriteLine("Please Enter The First Number: ");
+                        numString = Console.ReadLine();
+                        numberOne = int.Parse(numString);
+                        Console.WriteLine("Please Enter The Second Number: ");
+                        numString = Console.ReadLine();
+                        numberTwo = int.Parse(numString);
+                        Console.WriteLine("{0} + {1} = {2}", numberOne, numberTwo, numberOne + numberTwo);
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("invalid input please try again!");
+                    }
+
+                // subtracting two numbers
+                if (homeMenu == "2")
+                    try
+                    {
+                        Console.WriteLine("Please Enter The First Number: ");
+                        numString = Console.ReadLine();
+                        numberOne = int.Parse(numString);
+                        Console.WriteLine("Please Enter The Second Number: ");
+                        numString = Console.ReadLine();
+                        numberTwo = int.Parse(numString);
+                        Console.WriteLine("{0} - {1} = {2}", numberOne, numberTwo, numberOne - numberTwo);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR, you must have typed a incorrect function!");
+                    }
+
+                // dividing two numbers
+                if (homeMenu == "3")
+                    try
+                    {
+                        Console.WriteLine("Please Enter The First Number: ");
+                        numString = Console.ReadLine();
+                        numberOne = int.Parse(numString);
+                        Console.WriteLine("Please Enter The Second Number: ");
+                        numString = Console.ReadLine();
+                        numberTwo = int.Parse(numString);
+                        Console.WriteLine("{0} / {1} = {2}", numberOne, numberTwo, numberOne / numberTwo);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("WOOPS that didnt work please try again!");
+                    }
+
+                // multiplying two numbers
+                if (homeMenu == "4")
+                    try
+                    {
+                        Console.WriteLine("Please Enter The First Number: ");
+                        numString = Console.ReadLine();
+                        numberOne = int.Parse(numString);
+                        Console.WriteLine("Please Enter The Second Number: ");
+                        numString = Console.ReadLine();
+                        numberTwo = int.Parse(numString);
+                        Console.WriteLine("{0} * {1} = {2}", numberOne, numberTwo, numberOne * numberTwo);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Must be numbers not letters!");
+                    }
+
+            } while (true); 
+
+           
+
+        }
+
+    }
+}
+
