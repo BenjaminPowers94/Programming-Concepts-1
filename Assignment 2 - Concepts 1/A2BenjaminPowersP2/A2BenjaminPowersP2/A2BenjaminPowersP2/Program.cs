21ca9fa7d7a320f8edb56322c9fcf76ee117bdce
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 15:17:52 -0500

    Add files via upload

---
+++ Assignment 2 - Concepts 1/A2BenjaminPowersP2/A2BenjaminPowersP2/A2BenjaminPowersP2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Assignment 2 part 2
+// By Ben Powers
+// Student number 7986250 
+
+namespace A2BenjaminPowersP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.WriteLine("Ben Powers");
+                Console.WriteLine("------------------------------------------");
+
+                // Finding the volume of a sphere
+
+                Console.WriteLine("Finding the volume of a sphere ");
+                double r, vol;
+
+                Console.WriteLine("Insert the radius of the sphere");
+                r = Convert.ToDouble(Console.ReadLine());
+
+                // calculation 
+                vol = 4 * Math.PI * Math.Pow(r, 3) / 3;
+
+                // displaying the outcome of the calculation of a spheres volume 
+                Console.WriteLine("The volume of a sphere with the radius of {0} is {1:##.##}", r, vol);
+                Console.ReadLine();
+
+
+                // Finding the volume of a cylinder
+
+                Console.WriteLine("Finding the volume of a cylinder ");
+                double radius, height;
+
+                Console.Write("Enter radius: ");
+                radius = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter height: ");
+                height = double.Parse(Console.ReadLine());
+
+                // calculation
+                vol = Math.PI * Math.Pow(radius, 2) * height;
+
+                // displaying the outcome of the calculation of a cylinders volume
+                Console.WriteLine("The Volume of a cylinder with the radius {0} and height {1} is {2}", r, height, vol);
+                Console.ReadLine();
+
+
+                // Finding the volume of a Rectangular Prism
+
+                Console.WriteLine("Finding the volume of a Rectangular Prism");
+                double LengthOfRectangle, WidthOfRectangle, HeightOfRectangle;
+
+                Console.Write("Enter Length: ");
+                LengthOfRectangle = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter Width: ");
+                WidthOfRectangle = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter Height: ");
+                HeightOfRectangle = double.Parse(Console.ReadLine());
+
+                // calculation
+                vol = (LengthOfRectangle * WidthOfRectangle * HeightOfRectangle);
+
+                // displaying the calculation of a Rectangles volume
+                Console.WriteLine("The volume of the Rectangular Prism with the length {0} and width {1} and height {2} is {3}", LengthOfRectangle, WidthOfRectangle, HeightOfRectangle, vol); 
+                Console.ReadLine(); 
+
+            }
+
+        }
+    }
+}
---
+++ Assignment 2 - Concepts 1/A2BenjaminPowersp1/A2BenjaminPowersp1/A2BenjaminPowersp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Assignment 2 Part 1 
+// By Ben Powers
+// Student number 7986250 
+
+namespace A2BenjaminPowersP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string student;
+            int age, months;
+
+            int internationalFee = 0;
+            int baseTuitionFee = 0;
+            int registrationFee = 0;
+
+            double hst = 0;
+            double tuitionTotal = 0;
+            
+            Console.WriteLine("Ben Powers");
+            Console.WriteLine("------------------------------------------");
+
+
+            // International Fee
+            Console.WriteLine("Are you a Canadian or International student");
+            Console.WriteLine("Enter I for International");
+            student = Console.ReadLine();
+
+            if (student == "I")
+            {
+                internationalFee = 100;
+            }
+
+            // Basic Tuition Fee
+            Console.WriteLine("What is your age?");
+            age = int.Parse(Console.ReadLine());
+
+            if (age <= 18)
+            {
+                baseTuitionFee = 300;
+            }
+            if (age > 18 && age <= 49)
+            {
+                baseTuitionFee = 500;
+            }
+            else // age >= 50
+            {
+                baseTuitionFee = 400;
+            }
+
+            // Registration Fee
+            Console.WriteLine("What months did you register?");
+            Console.WriteLine("1.the Fall months (Sept., Oct., Nov., or Dec.)");
+            Console.WriteLine("2.the Winter months (Jan., Feb., Mar. or Apr.)");
+            Console.WriteLine("3.the Summer months (May, Jun., Jul. or Aug.)");
+
+            do
+            {
+                months = int.Parse(Console.ReadLine());
+
+                switch (months)
+                {
+                    case 1:
+                        registrationFee = 250;
+
+                        break;
+                    case 2:
+                        registrationFee = 220;
+                        break;
+                    case 3:
+                        registrationFee = 150;
+                        break;
+                    default:
+                        registrationFee = 0;
+                        break;
+                }
+            } while (registrationFee == 0);
+
+            // Caculate Total Tuition Fee
+            hst = (internationalFee + baseTuitionFee + registrationFee) * 0.13;  // 13% HST
+            tuitionTotal = internationalFee + baseTuitionFee + registrationFee + hst;
+
+
+            Console.WriteLine("Base Tuition : $" + baseTuitionFee);
+            Console.WriteLine("International Tuition Fee : $" + internationalFee);
+            Console.WriteLine("Registration Fee's : $" + registrationFee);
+            Console.WriteLine("HST Taxes : $" + hst);
+            Console.WriteLine("Final Price for student : $" + tuitionTotal);
+        }
+    }
+}
