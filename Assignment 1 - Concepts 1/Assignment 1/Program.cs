8ec6e81b86fdbcb3545c95a7e29aa366a21375a8
Benjamin Powers <41301379+BenjaminPowers94@users.noreply.github.com>
2019-01-04 15:16:28 -0500

    Add files via upload

---
+++ Assignment 1 - Concepts 1/Assignment 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// Project 1 By Ben Powers
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Ben Powers - Assignment 1 Part 2");
+            Console.ReadLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            Console.WriteLine("Name: Ben Powers");
+            Console.ReadLine();
+            Console.WriteLine("Email: bpowers6250@conestogac.on.ca");
+            Console.ReadLine();
+            Console.WriteLine("Program: CP");
+            Console.ReadLine();
+            Console.WriteLine("Experience: No previous programming experience");
+            Console.ReadLine();
+            Console.WriteLine("Expectation: To gain confidence and expeirence in the computer programming field");
+            Console.ReadLine();
+
+        }
+    }
+}
